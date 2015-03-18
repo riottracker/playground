9d2e2342799fd5683cde893c69495e6eeaede620
kohlrabi23 <kohlrabi23@users.noreply.github.com>
2015-03-18 16:19:44 +0100

    Update Keys.hs

--- testing/Keys.hs
+++ testing/Keys.hs
@@ -1,6 +1,7 @@
 module Keys (keyToFreq) where
 
 -- this is an awful makeshift solution
+-- maybe we could use something similar to Haskore.Basic.Pitch
 
 keyToFreq :: Double -> Double
 keyToFreq k = 440.0*2**((k-49.0)/12)
