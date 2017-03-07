6f6ffdc85c8ea67e463261d40adafd2c55d9400c
kohlrabi <kohlrabi@aragonit>
2017-03-07 09:51:07 +0100

    experiments with chords

--- testing/Main.hs
+++ testing/Main.hs
@@ -1,15 +1,37 @@
 import Stream
-import Oscillator
 import Play
-import Signal
 import Keys
 
 sampleRate :: Int
 sampleRate = 44100
 
+type Oscillator = Double -> Signal
+
+sine :: Oscillator
+sine f t = sin $ f * 2.0 * pi * t
+
+square :: Oscillator
+square f t
+  | sine f t < 0 = -1
+  | otherwise = 1
+
+mix :: Oscillator -> Oscillator -> Oscillator
+mix o1 o2 = \f t -> (o1 f t + o2 f t) * 0.5
+
+quint :: Oscillator -> Oscillator
+quint o = mix o (o . ((3/2)*))
+
+minor :: Oscillator -> Oscillator
+minor o = mix (quint o) (o . ((6/5)*))
+
+major :: Oscillator -> Oscillator
+major o = mix (quint o) (o . ((5/4)*))
+
+
 main = do
-   s <- loadStream "sample.wav" 1
-   let schnipsel = take 140000 s
-   playOnStdOut $ schnipsel ++ (reverse schnipsel) ++ pause
-   playOnStdOut $ concat $ map (renderSignal 0.0 0.4 sampleRate) $ map (sine . keyToFreq) [40..50]
-   where pause = renderSignal 0.0 0.05 sampleRate silence
+--   s <- loadStream "sample.wav" 1
+--   let schnipsel = take 140000 s
+--   playOnStdOut $ schnipsel ++ (reverse schnipsel) ++ pause
+--   playOnStdOut $ concat $ map (renderSignal 0.0 0.4 sampleRate) $ map (sine . keyToFreq) [40..50]
+-- where pause = renderSignal 0.0 0.05 sampleRate silence
+     playOnStdOut $ renderSignal 0.0 1.0 sampleRate (major square 440)
--- testing/Stream.hs
+++ testing/Stream.hs
@@ -1,11 +1,22 @@
-module Stream (Stream, loadStream) where
+module Stream (Stream, Signal, renderSignal, loadStream) where
 
 import Data.Int (Int32 (..))
 import Data.WAVE
 
+type Signal = Double -> Double
 type Stream = [Int32]
 
 loadStream :: String -> Int -> IO Stream
 loadStream fn ch = do
    s <- getWAVEFile fn
    return $ map (!! ch) (waveSamples s)
+
+
+renderSignal :: Double -> Double -> Int -> Signal -> Stream
+renderSignal startPosition endPosition sampleRate s = [(toStream . clipped) (sample * period) | sample <- [0..total]]
+   where toStream x = (truncate $ minSig + ((x + 1.0) / 2.0 * (maxSig - minSig)))
+         minSig = fromIntegral (minBound :: Int32)
+         maxSig = fromIntegral (maxBound :: Int32)
+         clipped = (max (-1.0) . min 1.0 . s)
+         total = (endPosition - startPosition) / period
+         period = 1.0 / (fromIntegral sampleRate)
