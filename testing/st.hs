f08345df5f9d422e5458c3ac1f808cae1b193ac3
kohlrabi <kohlrabi@aragonit>
2017-03-13 22:40:33 +0100

    midi instrument experiments

---
+++ testing/st.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE RecordWildCards #-}
+
+import Data.Maybe
+import Stream
+import Play
+import Keys
+import Synth
+import Instrument
+import Event
+import qualified Data.Map as M
+import Foreign.C.Types
+import Control.Concurrent
+import Sound.RtMidi
+
+sampleRate :: Int
+sampleRate = 44100
+
+
+main = do
+  device <- defaultInput
+  openPort device 0 "Synthesizer test"
+  mainLoop (Instrument bell) device
+
+eventFromMessage :: [CUChar] -> Maybe Event
+eventFromMessage [0x90, note, _] = Just $ NoteOn (fromEnum note)
+eventFromMessage _               = Nothing
+
+mainLoop :: Instrument -> Device -> IO ()  
+mainLoop i d = do
+  m <- getMessage d
+  let ni = if (length (fst m) > 0) then
+        (case eventFromMessage (fst m) of
+           Just e  -> handleEvent e i
+           Nothing -> i
+        )
+       else i
+  let p = render (snd m) sampleRate ni
+  playOnStdOut (snd p)
+--  if (length $ fst m) > 0 then do
+--    putStrLn $ show m
+--    mainLoop (fst p) d
+--  else
+  threadDelay 200
+  mainLoop (fst p) d
