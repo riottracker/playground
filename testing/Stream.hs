435b90cde879afbe88c3971d4a631e095d5660dc
kohlrabi <kohlrabi@aragonit>
2017-03-15 20:34:29 +0100

    new function to render single frames

--- testing/Stream.hs
+++ testing/Stream.hs
@@ -1,4 +1,4 @@
-module Stream (Stream, Signal, renderSignal, silence) where
+module Stream (Stream, Signal, renderFrame, renderSignal, silence) where
 
 import Data.Int (Int32 (..))
 
@@ -8,12 +8,17 @@ type Stream = [Int32]
 silence :: Signal
 silence = const 0
 
-renderSignal :: Double -> Double -> Int -> Signal -> Stream
-renderSignal startPosition endPosition sampleRate s = [(toStream . clipped) (sample * period) | sample <- [start..end]]
+renderFrame :: Int -> Signal -> Int -> Int32
+renderFrame sampleRate s position = toStream . clipped $ (fromIntegral position) * period
    where toStream x = (truncate $ minSig + ((x + 1.0) / 2.0 * (maxSig - minSig)))
          minSig = fromIntegral (minBound :: Int32)
          maxSig = fromIntegral (maxBound :: Int32)
          clipped = (max (-1.0) . min 1.0 . s)
-         start = startPosition / period
+         period = 1.0 / (fromIntegral sampleRate)
+ 
+
+renderSignal :: Double -> Double -> Int -> Signal -> Stream
+renderSignal startPosition endPosition sampleRate s = [ renderFrame sampleRate s $ floor  sample | sample <- [start..end] ]
+   where start = startPosition / period
          end = endPosition / period
          period = 1.0 / (fromIntegral sampleRate)
