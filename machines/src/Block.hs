9fc3477ddc367fb16b6c1f04686fe70d30a88dfe
kohlrabi <kohlrabi@aragonit>
2017-09-27 15:26:31 +0200

    memory optimisations

--- machines/src/Block.hs
+++ machines/src/Block.hs
@@ -1,16 +1,22 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE Rank2Types #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections #-}
 
 module Block where
 
+import Control.Monad
+import Control.Monad.ST
+import Data.Array.ST
+import Data.STRef
 import Data.Tuple
+import Debug.Trace (traceM)
 
 sampleRate :: Int
 sampleRate = 44100
 
 class Block a where
-  eval :: (a, Double) -> Buffer -> Int -> (a, Double)
+  eval :: Buffer s -> Int -> (a, Double) -> ST s (a, Double)
 
 data Module = forall a . (Show a, Block a) => Module (a,Double)
 
@@ -20,21 +26,23 @@ output (Module (a,b)) = b
 instance Show Module where
   show (Module (a,b)) = show (a,b)
 
-type Buffer = [Module]
+type Buffer s = STArray s Int Module
 
-data Network = Network { buffer :: Buffer
-                       , sink   :: Lambda
-                       , params :: [Int]
-                       , t      :: Int
-                       }
+data Network s = Network { buffer :: Buffer s
+                         , sink   :: Lambda
+                         , params :: [Int]
+                         , t      :: STRef s Int
+                         }
 
-step :: Network -> (Network, Double)
-step n@Network{..} =
-  (n { buffer = fmap (\(Module a) -> Module $ eval a buffer (t+1)) buffer
-     , t = t + 1
-     }
-  , evalLambda sink buffer
-  )
+evalElem :: Buffer s -> Int -> Int -> ST s Module
+evalElem b t n = readArray b n >>= \(Module e) -> Module <$> eval b t e
+ 
+step :: Network s -> ST s (Network s)
+step n@Network{..} = do
+   k <- readSTRef t
+   mapM_ (evalElem buffer k) =<< range <$> getBounds buffer
+   modifySTRef' t (+1)
+   return n
 
 data Lambda = Lambda Lambda Lambda (Double -> Double -> Double) | Var Int | Const Double
 
@@ -44,25 +52,24 @@ instance Show Lambda where
   show (Const  d) = "Const. " ++ show d
 
 instance Block Lambda where
-  eval (l,_) b _ = (l, evalLambda l b)
+  eval b _ (l,_) = (l ,) <$> evalLambda l b
 
-evalLambda :: Lambda -> Buffer -> Double
-evalLambda (Const d) _      = d
-evalLambda (Var   n) b      = output (b !! n)
-evalLambda (Lambda l r f) b = f (evalLambda l b) (evalLambda r b)
+evalLambda :: Lambda -> Buffer s -> ST s Double
+evalLambda (Const d) _      = return d
+evalLambda (Var   n) b      = output <$> readArray b n
+evalLambda (Lambda l r f) b = liftM2 f (evalLambda l b) (evalLambda r b)
 
 newtype Param = Param String
   deriving (Show,Eq)
 
 instance Block Param where
-  eval (p,o) _ _ = (p,o)
+  eval _ _ (p,o) = return (p,o)
 
 data Sine = Sine { freq :: Lambda }
   deriving (Show)
 
 instance Block Sine where
-  eval (Sine freq, _) b t =
-      (Sine freq, sin $ pi * 2.0 * f / fromIntegral sampleRate)
-    where f = evalLambda freq b
-
-
+    eval b t (Sine freq, _) = do
+      f <- evalLambda freq b
+      return (Sine freq, fromIntegral t)
+      --return (Sine freq, sin $ pi * 2.0 * f / fromIntegral sampleRate)
--- machines/src/Main.hs
+++ machines/src/Main.hs
@@ -1,53 +1,39 @@
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Main where
 
+import           Control.Monad.ST
+import           Data.Array.ST
+import           Data.STRef
 import           Data.Binary
 import qualified Data.ByteString.Lazy as B
-import qualified Data.Machine as M
-import qualified Data.Machine.Mealy as M
-import           Data.Int (Int16 (..))
+import           Data.Int (Int8 (..))
+import           Block
 
-sampleRate :: Int
-sampleRate = 44100
+net :: ST s (Network s)
+net = do
+  a <- newListArray (0,0) [Module (Sine $ Const 440, 0)]
+  n <- newSTRef 0
+  return $ Network a (Var 0) [] n
 
-oscillator :: Double -> (Double -> Double) -> M.Source Double
-oscillator freq w = M.source [ w $ t * pi * 2.0 * (1.0 / fromIntegral sampleRate) | t <- [1.. fromIntegral sampleRate*10] ]
+doubleToSample :: Double -> Int8
+doubleToSample v =
+    let maxb = fromIntegral (maxBound :: Int8)
+        minb = fromIntegral (minBound :: Int8) in
+    if v >= 0
+    then (fromIntegral . floor . (* maxb)) (min v 1)
+    else (fromIntegral . ceiling . (* minb)) (min (-v) 1)
 
-tremolo :: M.Tee Double Double Double
-tremolo = M.zipWith (*)
-
-c :: Double -> Int16
-c x = (truncate $ minSig + ((clipped + 1.0) / 2.0 * (maxSig - minSig)))
- where clipped = (max (-1.0) . min 1.0 $ x)
-       minSig  = fromIntegral (minBound :: Int16)
-       maxSig  = fromIntegral (maxBound :: Int16)
-
-
-output = M.mapping c
-
---example = output M.<~ M.tee (oscillator 100 sin) (oscillator 400 sin) tremolo
-example = output M.<~ (M.auto $ filt 640 0.0) M.<~ (oscillator 100 sin)
-
-filt :: Double -> Double -> M.Mealy Double Double
-filt cutoff res = M.unfoldMealy (vcf cutoff res) (0,0,0,0,0)
-
-type FilterState = (Double, Double, Double, Double, Double)
-
-vcf :: Double -> Double -> FilterState -> Double -> (Double, FilterState)
-vcf cutoff res (x,y1,y2,y3,y4) input = (out, (x',y1',y2',y3',y4'))
-  where x' = input - r*y4
-        y1' = p*(x' + x)   - k*y1
-        y2' = p*(y1 + y1') - x*y2
-        y3' = p*(y2 + y2') - x*y3
-        y4' = p*(y3 + y3') - x*y4
-        out = y4 - (y4*y4*y4)/6.0
-        f  = 2 * cutoff / 44100
-        k  = 3.6 * f - 1.6*f*f - 1
-        p  = (k+1)*0.5
-        scale = (exp 1)**((1-p)*1.4)
-        r  = res * scale
-  
+render :: Int -> Network s -> ST s [Double]
+render 0   _ = return []
+render n net@Network{..} = do
+    net' <- step net
+    frame <- evalLambda sink buffer
+    frames <- render (n-1) net'
+    return $ frame:frames 
+   
 main :: IO ()
-main = B.putStr . encode $ M.run example
+main = mapM_ print $ runST (net >>= (render $ sampleRate * 2))
+ 
 
